05681986a2c3c4c310355a11c7725289aef199d9
魏俊杰 <weijunjie@rainbowcn.com>
2021-09-24 16:40:21 +0800

    部署到服务器

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "vite-h5",
+      script: "vite-h5-server.js",
+    },
+  ],
+  deploy: {
+    production: {
+      user: "root",
+      host: "175.24.112.44",
+      ref: "origin/master",
+      repo: "git@github.com:rrr5t6y7/viteReactPro.git",
+      path: "/home/nginx/www/",
+      "post-deploy":
+        "git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js", // -production=false 下载全量包
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  },
+};
