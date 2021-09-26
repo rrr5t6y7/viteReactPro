711e7d6300f2c56a4ff26334a67a89e824a76c45
魏俊杰 <weijunjie@rainbowcn.com>
2021-09-26 16:22:11 +0800

    暂时去除所有icon

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,8 +4,7 @@ import { Toast } from "zarm";
 
 const MODE = import.meta.env.MODE; // 环境变量
 
-// axios.defaults.baseURL =
-//   MODE == "development" ? "/api" : "http://api.chennick.wang";
+axios.defaults.baseURL = MODE == "development" ? "/api" : "/v1/api";
 axios.defaults.withCredentials = true;
 axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.headers["Authorization"] = `${
